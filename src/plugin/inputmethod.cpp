78ef16e46f2b1e5b1707b95743ce6e37d70656a0
Christophe Chapuis <chris.chapuis@gmail.com>
2014-02-08 12:24:29 +0100

    plugin: fix height resize of the view
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- src/plugin/inputmethod.cpp
+++ src/plugin/inputmethod.cpp
@@ -505,7 +505,7 @@ void InputMethod::onVisibleRectChanged()
 
     inputMethodHost()->setScreenRegion(QRegion(visibleRect));
     inputMethodHost()->setInputMethodArea(visibleRect, d->view);
-    d->view->setHeight(visibleRect.height());
+    d->view->setHeight(d->m_geometry->visibleRect().toRect().height());
 
     qDebug() << "keyboard is reporting <x y w h>: <"
                 << visibleRect.x()
