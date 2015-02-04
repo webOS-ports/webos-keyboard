cbe417ff0d2821bd5b06fc580494dfca237335bb
Christophe Chapuis <chris.chapuis@gmail.com>
2015-02-04 21:53:48 +0100

    Use a mask to avoid preventing interaction on screen zones just above the keyboard
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- src/plugin/inputmethod.cpp
+++ src/plugin/inputmethod.cpp
@@ -492,6 +492,7 @@ void InputMethod::onVisibleRectChanged()
     inputMethodHost()->setScreenRegion(QRegion(visibleRect));
     inputMethodHost()->setInputMethodArea(visibleRect, d->view);
     d->view->setHeight(d->m_geometry->canvasHeight());
+    d->view->setMask(QRegion(visibleRect));
 
     qDebug() << "keyboard is reporting <x y w h>: <"
                 << visibleRect.x()
