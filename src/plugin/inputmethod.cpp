webOS-ports/webos-keyboard

commit 32f6db5bca2f0982452f96a076dfc2034747c445

pluginmethod: reintroduced some fixes still needed.
Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

src/plugin/inputmethod.cpp
@@ -147,6 +147,11 @@ void InputMethod::hide()
     d->closeOskWindow();
 }
 
+void InputMethod::reset()
+{
+    hide();
+}
+
 void InputMethod::setPreedit(const QString &preedit,
                              int cursor_position)
 {
@@ -166,7 +171,14 @@ QList<MAbstractInputMethod::MInputMethodSubView>
 InputMethod::subViews(Maliit::HandlerState state) const
 {
     Q_UNUSED(state)
+    Q_D(const InputMethod);
+
     QList<MInputMethodSubView> views;
+
+	MInputMethodSubView v;
+	v.subViewId = d->activeLanguage;
+	views.append(v);
+
     return views;
 }
 
@@ -176,6 +188,8 @@ void InputMethod::setActiveSubView(const QString &id,
 {
     Q_UNUSED(state)
     Q_UNUSED(id);
+
+	setActiveLanguage(id);
 }
 
 QString InputMethod::activeSubView(Maliit::HandlerState state) const

commit 818db5710f9ae930644562a7ce7999ccc7125fb6

inputmethod: don't hide at reset
reset is supposed to reset the internal state, but not to change
the current hide/show status.

This mistake was introduced in 32f6db5bca2f0982452f96a076dfc2034747c445

Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

src/plugin/inputmethod.cpp
@@ -159,7 +159,6 @@ void InputMethod::hide()
 
 void InputMethod::reset()
 {
-    hide();
 }
 
 void InputMethod::setPreedit(const QString &preedit,